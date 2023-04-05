c3b18cec8a2be9784e4b5818b44583ba2552c931
fekoneko <55813967+fekoneko@users.noreply.github.com>
2023-04-05 14:27:20 +0300

    fix footer display bugs

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
-import { useLocation } from 'react-router-dom';
 import globalContext from '../contexts/globalContext';
 import kanjiContext, { Kanji } from '../contexts/kanjiContext';
 import {
@@ -10,22 +9,24 @@ import {
 } from '../controllers/kanjiController';
 
 const ControlPanel = () => {
-  const { section } = useContext(globalContext);
-  const location = useLocation();
+  const { section, inSectionPath } = useContext(globalContext);
   const { popularKanjis, savedKanjis, searchKanjis, selectedKanjis, setSelectedKanjis } =
     useContext(kanjiContext);
 
   let displayedKanjis: null | Kanji[] = null;
   if (section === 'main') {
-    switch (location.pathname) {
-      case '/popular':
+    switch (inSectionPath) {
+      case 'popular':
         displayedKanjis = popularKanjis;
         break;
-      case '/saved':
+      case 'saved':
         displayedKanjis = savedKanjis;
         break;
-      case '/search':
+      case 'search':
         displayedKanjis = searchKanjis;
+        break;
+      case 'selected':
+        displayedKanjis = selectedKanjis;
         break;
     }
   }
@@ -44,32 +45,34 @@ const ControlPanel = () => {
     setSelectedKanjis([]);
   };
 
-  return (
-    <div className="controlPanel">
-      {section === 'main' ? (
-        location.pathname !== '/selected' ? (
-          <>
-            <p>{`выделено ${
-              displayedKanjis ? getCountOfKanjisInList(selectedKanjis, displayedKanjis) : '?'
-            } из ${displayedKanjis?.length}`}</p>
-            {displayedKanjis && isKanjisInList(selectedKanjis, displayedKanjis) ? (
-              <button onClick={deselectDisplayed}>снять выделение</button>
-            ) : (
-              <button onClick={selectDisplayed}>выделить всё</button>
-            )}
-          </>
+  if (inSectionPath)
+    return (
+      <div className="controlPanel">
+        {section === 'main' ? (
+          inSectionPath !== 'selected' ? (
+            <>
+              <p>{`выделено ${
+                displayedKanjis ? getCountOfKanjisInList(selectedKanjis, displayedKanjis) : '?'
+              } из ${displayedKanjis?.length}`}</p>
+              {displayedKanjis && isKanjisInList(selectedKanjis, displayedKanjis) ? (
+                <button onClick={deselectDisplayed}>снять выделение</button>
+              ) : (
+                <button onClick={selectDisplayed}>выделить всё</button>
+              )}
+            </>
+          ) : (
+            <>
+              <p>{`выделено ${selectedKanjis ? selectedKanjis.length : '?'}`}</p>
+              <button onClick={clearSelection}>очистить список</button>
+            </>
+          )
         ) : (
-          <>
-            <p>{`выделено ${selectedKanjis ? selectedKanjis.length : '?'}`}</p>
-            <button onClick={clearSelection}>очистить список</button>
-          </>
-        )
-      ) : (
-        <p style={{ textAlign: 'center' }}>
-          Используйте стрелки для перемещения. Пробел переворачивает карточку
-        </p>
-      )}
-    </div>
-  );
+          <p style={{ textAlign: 'center' }}>
+            Используйте стрелки для перемещения. Пробел переворачивает карточку
+          </p>
+        )}
+      </div>
+    );
+  else return null;
 };
 export default ControlPanel;
--- src/contexts/globalContext.tsx
+++ src/contexts/globalContext.tsx
@@ -10,26 +10,53 @@ type Auth = {
 
 type GlobalContextValue = {
   section: Section;
+  inSectionPath: InSectionPath;
   auth: Auth | null;
   setAuth: React.Dispatch<React.SetStateAction<Auth | null>>;
 };
 type GlobalContextProviderProps = { children: ReactNode };
 type Section = 'learn' | 'main';
+type InSectionPath =
+  | 'popular'
+  | 'saved'
+  | 'search'
+  | 'selected'
+  | 'by-meaning'
+  | 'by-writing'
+  | null;
+
+const IN_SECTION_PATHES = ['popular', 'saved', 'search', 'selected', 'by-meaning', 'by-writing'];
 
 const globalContext = createContext({} as GlobalContextValue);
 
 export const GlobalContextProvider = ({ children }: GlobalContextProviderProps) => {
   const [section, setSection] = useState<Section>('main');
+  const [inSectionPath, setInSectionPath] = useState<InSectionPath>(null);
   const location = useLocation();
   const [auth, setAuth] = useState<Auth | null>(null);
 
   useEffect(() => {
-    if (location.pathname.indexOf('/learn') === 0) setSection('learn');
-    else setSection('main');
+    const getSection = (): Section => {
+      if (location.pathname.indexOf('/learn') === 0) return 'learn';
+      else return 'main';
+    };
+    const getInSectionPath = (section: Section): InSectionPath => {
+      const uncheckedPath = location.pathname.substring(section === 'learn' ? 7 : 1);
+      console.log(uncheckedPath);
+      if (IN_SECTION_PATHES.includes(uncheckedPath)) return uncheckedPath as InSectionPath;
+      else return null;
+    };
+
+    const newSection = getSection();
+    setSection(newSection);
+    const newInSectionPath = getInSectionPath(newSection);
+    setInSectionPath(newInSectionPath);
   }, [location.pathname]);
 
   return (
-    <globalContext.Provider value={{ section, auth, setAuth }}>{children}</globalContext.Provider>
+    <globalContext.Provider value={{ section, inSectionPath, auth, setAuth }}>
+      {children}
+    </globalContext.Provider>
   );
 };
 
