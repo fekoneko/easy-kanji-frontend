22af005d840c8568018fccd4146cc74f4efc9410
fekoneko <55813967+fekoneko@users.noreply.github.com>
2023-06-01 23:04:50 +0300

    vite-config for deploy and style fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
 });
